07d1a0f7d88c935828d28c77f464e736762bf60b
cman8564134 <clement_18@live.com.my>
2021-04-16 20:20:21 +0800

    initial commit

---
+++ src/main/java/com/motional/cthye/taxibookingsystem/TaxibookingsystemApplication.java
@@ -0,0 +1,13 @@
+package com.motional.cthye.taxibookingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxibookingsystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaxibookingsystemApplication.class, args);
+    }
+
+}
